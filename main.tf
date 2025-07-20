955a7e590d8369ad497d05d9d2b494513500272a
peter chege <emchegep@gmail.com>
2025-07-20 22:50:55 +0300

    configured a cluster of web servers using Auto Scaling Group

--- main.tf
+++ main.tf
@@ -1,5 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 provider "aws" {
     region = "us-east-2"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_security_group" "my-security-group" {
@@ -14,10 +33,10 @@ resource "aws_security_group" "my-security-group" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0d1b5a8c13042c939"
+resource "aws_launch_configuration" "asg-launch-config" {
+  image_id      = "ami-0d1b5a8c13042c939"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.my-security-group.id]
+  security_groups = [aws_security_group.my-security-group.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -25,9 +44,22 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  user_data_replace_on_change = true
+  # Required when using a launch configuration with an auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "asg" {
+  launch_configuration = aws_launch_configuration.asg-launch-config.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
