dbf6610de60dd8e7ecbe7174650885738ef88a44
peter chege <emchegep@gmail.com>
2025-07-21 10:49:12 +0300

    deployed a cluster of web servers using ASG and Load Balancer

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
 provider "aws" {
     region = "us-east-2"
 }
@@ -33,16 +25,18 @@ resource "aws_security_group" "my-security-group" {
   }
 }
 
-resource "aws_launch_configuration" "asg-launch-config" {
+resource "aws_launch_template" "asg-launch-template" {
+  name   = "asg-launch-template"
   image_id      = "ami-0d1b5a8c13042c939"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.my-security-group.id]
+  vpc_security_group_ids = [aws_security_group.my-security-group.id]
 
-  user_data = <<-EOF
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  )
 
   # Required when using a launch configuration with an auto scaling group
   lifecycle {
@@ -51,15 +45,102 @@ resource "aws_launch_configuration" "asg-launch-config" {
 }
 
 resource "aws_autoscaling_group" "asg" {
-  launch_configuration = aws_launch_configuration.asg-launch-config.name
+  name = "terraform-asg-example"
+  launch_template {
+    id      = aws_launch_template.asg-launch-template.id
+    version = "$Latest"
+  }
   vpc_zone_identifier = data.aws_subnets.default.ids
-
   min_size = 2
-  max_size = 10
+  max_size = 4
+  desired_capacity = 3
+
+  target_group_arns = [aws_lb_target_group.asg-target-group.arn]
+  health_check_type = "ELB"
+
+
 
   tag {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "my-alb" {
+  name               = "my-alb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb-security-group.id]
+}
+
+resource "aws_lb_listener" "my-alb-listener" {
+  load_balancer_arn = aws_lb.my-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb-security-group" {
+  name        = "alb-security-group"
+  description = "ALB security group"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg-target-group" {
+  name     = "asg-target-group"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg-listener-rule" {
+  listener_arn = aws_lb_listener.my-alb-listener.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg-target-group.arn
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "webserver_public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+    value = aws_lb.my-alb.dns_name
+    description = "The domain name of the load balancer"
 }
